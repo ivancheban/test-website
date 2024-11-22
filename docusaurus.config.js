39c381a21af7cac167f74b9fab8bb1b842620c0b
Ivan Cheban <ivancheban@gmail.com>
2024-11-22 21:26:35 +0200

    chnaged url field

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ivancheban.github.io',
+  url: 'https://ivancheban.github.io/test-website',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
